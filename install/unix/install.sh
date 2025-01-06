7cb4fadfad6af11d810e56c13d9c8a9b17632b41
rikardbq <j10rikardb@gmail.com>
2025-01-06 22:10:45 +0100

    add install script for unix and change move to copy

---
+++ install/unix/install.sh
@@ -0,0 +1,33 @@
+# INSTALL
+echo "[RUNNING::INSTALL]"
+
+SERF_ROOT_DIR="$HOME/.serf"
+TARGET_DIR="$PWD/target/release"
+
+while getopts td:rd: flag
+do
+    case "${flag}" in
+        rd) SERF_ROOT_DIR=${OPTARG};;
+    esac
+done
+
+echo "[RUNNING::BUILD] ROOT=$SERF_ROOT_DIR"
+echo "[BUILD_TARGET] ARTIFACTS=$TARGET_DIR"
+
+cargo build --release
+
+echo "[COMPLETE::BUILD]"
+echo "[RUNNING::COPY_EXECUTABLES] ROOT=$SERF_ROOT_DIR"
+
+# check dir existence and make if not exist
+if [ ! -d "$SERF_ROOT_DIR" ]; then
+    mkdir $SERF_ROOT_DIR
+fi
+
+cp -t $SERF_ROOT_DIR $TARGET_DIR/sqlite_server_srv $TARGET_DIR/sqlite_server_cli
+
+echo "[COMPLETE::COPY_EXECUTABLES]"
+echo "[COMPLETE::INSTALL]"
+
+exit 0
+
